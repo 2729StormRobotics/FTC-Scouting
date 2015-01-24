d8b571aa83695c559ec2e1992e62c25791467170
atf1999 <torth212@gmail.com>
2015-01-24 11:01:03 -0500

    Continued Working on the app

--- StormRadar/app/src/main/java/storm/ftc/stormradar/MainActivity.java
+++ StormRadar/app/src/main/java/storm/ftc/stormradar/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import storm.ftc.stormradar.DataHandler.Datahandler;
 
@@ -18,6 +19,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText team = (EditText) this.findViewById(R.id.team),
+                team2 = (EditText) this.findViewById(R.id.team2),
+                match = (EditText) this.findViewById(R.id.match),
+                match2 = (EditText) this.findViewById(R.id.match2);
+
 
     }
     public boolean DataEntered() {
@@ -26,14 +32,10 @@ public class MainActivity extends ActionBarActivity {
                 match = (EditText) this.findViewById(R.id.match),
                 match2 = (EditText) this.findViewById(R.id.match2);
 
-        RadioButton red = (RadioButton) this.findViewById(R.id.btn_red),
-                red2 = (RadioButton) this.findViewById(R.id.btn_red2),
-                blue = (RadioButton) this.findViewById(R.id.btn_blue),
+        RadioButton red2 = (RadioButton) this.findViewById(R.id.btn_red2),
                 blue2 = (RadioButton) this.findViewById(R.id.btn_blue2);
         if (!team.getText().toString().matches("") && !team2.getText().toString().matches("") && match.getText().toString().matches("")
-                && match2.getText().toString().matches("") && ((red.isChecked() && red2.isChecked())
-                || (red.isChecked() && blue2.isChecked()) || (red2.isChecked() && blue.isChecked())
-                || (blue.isChecked() && blue2.isChecked()))) {
+                && match2.getText().toString().matches("") && ((red2.isChecked() || blue2.isChecked()))) {
             return true;
         }
         return false;
@@ -45,16 +47,15 @@ public class MainActivity extends ActionBarActivity {
                   match = (EditText) this.findViewById(R.id.match),
                   match2 = (EditText) this.findViewById(R.id.match2);
 
-          RadioButton red = (RadioButton) this.findViewById(R.id.btn_red),
+          RadioButton
                   red2 = (RadioButton) this.findViewById(R.id.btn_red2),
-                  blue = (RadioButton) this.findViewById(R.id.btn_blue),
                   blue2 = (RadioButton) this.findViewById(R.id.btn_blue2);
 
           Datahandler.team = team.getText().toString();
           Datahandler.team2 = team2.getText().toString();
           Datahandler.match = match.getText().toString();
           Datahandler.match2 = match2.getText().toString();
-          Datahandler.isRed = red.isChecked();
+          Datahandler.isRed = red2.isChecked();
           Datahandler.isRed2 = red2.isChecked();
 
           Intent intent = new Intent(this, MatchActivity.class);
@@ -65,6 +66,9 @@ public class MainActivity extends ActionBarActivity {
           intent.putExtra("Alliance1", Datahandler.isRed);
           intent.putExtra("Alliance2", Datahandler.isRed2);
           startActivity(intent);
+      }
+        else{
+          Toast.makeText(this, "Please input all data", Toast.LENGTH_SHORT);
       }
 
     }
